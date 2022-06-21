d10d2d063be12cce28b441b4973951a8a6cbbbf2
Matt Schuchard <grapheneowns@gmail.com>
2022-06-21 09:48:40 -0400

    adding unit tests

---
+++ spec/linter-circle-spec.js
@@ -0,0 +1,71 @@
+'use babel';
+
+import * as path from 'path';
+
+describe('The Actions provider for Linter', () => {
+  const lint = require(path.join(__dirname, '../lib/main.js')).provideLinter().lint;
+
+  beforeEach(() => {
+    atom.workspace.destroyActivePaneItem();
+    waitsForPromise(() => {
+      atom.packages.activatePackage('linter-github-actions');
+      return atom.packages.activatePackage('language-yaml').then(() =>
+        atom.workspace.open(path.join(__dirname, 'fixtures/clean_workflows', 'ci.yaml'))
+      );
+    });
+  });
+
+  describe('checks a file with actions syntax issues', () => {
+    let editor = null;
+    const badFile = path.join(__dirname, 'fixtures/errors_workflows', 'ci.yaml');
+    beforeEach(() => {
+      waitsForPromise(() =>
+        atom.workspace.open(badFile).then(openEditor => {
+          editor = openEditor;
+        })
+      );
+    });
+
+    it('finds the messages', () => {
+      waitsForPromise(() =>
+        lint(editor).then(messages => {
+          expect(messages.length).toEqual(2);
+        })
+      );
+    });
+
+    it('verifies the messages', () => {
+      waitsForPromise(() => {
+        return lint(editor).then(messages => {
+          expect(messages[0].severity).toBeDefined();
+          expect(messages[0].severity).toEqual('error');
+          expect(messages[0].excerpt).toBeDefined();
+          expect(messages[0].excerpt).toEqual('"runs-on" section is missing in job "the_job"');
+          expect(messages[0].location.file).toBeDefined();
+          expect(messages[0].location.file).toMatch(/.+errors_workflows\/ci\.yaml$/);
+          expect(messages[0].location.position).toBeDefined();
+          expect(messages[0].location.position).toEqual([[4, 2], [4, 3]]);
+          expect(messages[1].severity).toBeDefined();
+          expect(messages[1].severity).toEqual('error');
+          expect(messages[1].excerpt).toBeDefined();
+          expect(messages[1].excerpt).toEqual('unexpected key "runs_on" for "job" section. expected one of "concurrency", "container", "continue-on-error", "defaults", "env", "environment", "if", "name", "needs", "outputs", "permissions", "runs-on", "secrets", "services", "steps", "strategy", "timeout-minutes", "uses", "with"');
+          expect(messages[1].location.file).toBeDefined();
+          expect(messages[1].location.file).toMatch(/.+errors_workflows\/ci\.yaml$/);
+          expect(messages[1].location.position).toBeDefined();
+          expect(messages[1].location.position).toEqual([[5, 4], [5, 5]]);
+        });
+      });
+    });
+  });
+
+  it('finds nothing wrong with a valid file', () => {
+    waitsForPromise(() => {
+      const goodFile = path.join(__dirname, 'fixtures/clean_workflows', 'ci.yaml');
+      return atom.workspace.open(goodFile).then(editor =>
+        lint(editor).then(messages => {
+          expect(messages.length).toEqual(0);
+        })
+      );
+    });
+  });
+});
