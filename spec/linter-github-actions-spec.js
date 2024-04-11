a24407a1c700fcbd3dbacef939bdff262823febc
Matt Schuchard <grapheneowns@gmail.com>
2024-04-11 09:20:43 -0400

    update unit test pathing for fixture relocation

--- spec/linter-github-actions-spec.js
+++ spec/linter-github-actions-spec.js
@@ -10,14 +10,14 @@ describe('The Actions provider for Linter', () => {
     waitsForPromise(() => {
       atom.packages.activatePackage('linter-github-actions');
       return atom.packages.activatePackage('language-yaml').then(() =>
-        atom.workspace.open(path.join(__dirname, 'fixtures/clean_workflows', 'ci.yaml'))
+        atom.workspace.open(path.join(__dirname, 'fixtures/.github/workflows', 'clean.yaml'))
       );
     });
   });
 
   describe('checks a file with actions syntax issues', () => {
     let editor = null;
-    const badFile = path.join(__dirname, 'fixtures/errors_workflows', 'ci.yaml');
+    const badFile = path.join(__dirname, 'fixtures/.github/workflows', 'errors.yaml');
     beforeEach(() => {
       waitsForPromise(() =>
         atom.workspace.open(badFile).then(openEditor => {
@@ -42,7 +42,7 @@ describe('The Actions provider for Linter', () => {
           expect(messages[0].excerpt).toBeDefined();
           expect(messages[0].excerpt).toEqual('"on" section is missing in workflow');
           expect(messages[0].location.file).toBeDefined();
-          expect(messages[0].location.file).toMatch(/.+errors_workflows\/ci\.yaml$/);
+          expect(messages[0].location.file).toMatch(/.+.github/workflows\/errors\.yaml$/);
           expect(messages[0].location.position).toBeDefined();
           expect(messages[0].location.position).toEqual([[0, 0], [0, 1]]);
           expect(messages[1].severity).toBeDefined();
@@ -50,7 +50,7 @@ describe('The Actions provider for Linter', () => {
           expect(messages[1].excerpt).toBeDefined();
           expect(messages[1].excerpt).toEqual('"runs-on" section is missing in job "the_job"');
           expect(messages[1].location.file).toBeDefined();
-          expect(messages[1].location.file).toMatch(/.+errors_workflows\/ci\.yaml$/);
+          expect(messages[1].location.file).toMatch(/.+.github/workflows\/errors\.yaml$/);
           expect(messages[1].location.position).toBeDefined();
           expect(messages[1].location.position).toEqual([[4, 2], [4, 3]]);
           expect(messages[2].severity).toBeDefined();
@@ -58,7 +58,7 @@ describe('The Actions provider for Linter', () => {
           expect(messages[2].excerpt).toBeDefined();
           expect(messages[2].excerpt).toEqual('unexpected key "runs_on" for "job" section. expected one of "concurrency", "container", "continue-on-error", "defaults", "env", "environment", "if", "name", "needs", "outputs", "permissions", "runs-on", "secrets", "services", "steps", "strategy", "timeout-minutes", "uses", "with"');
           expect(messages[2].location.file).toBeDefined();
-          expect(messages[2].location.file).toMatch(/.+errors_workflows\/ci\.yaml$/);
+          expect(messages[2].location.file).toMatch(/.+.github/workflows\/errors\.yaml$/);
           expect(messages[2].location.position).toBeDefined();
           expect(messages[2].location.position).toEqual([[5, 4], [5, 5]]);
         });
@@ -68,7 +68,7 @@ describe('The Actions provider for Linter', () => {
 
   it('finds nothing wrong with a valid file', () => {
     waitsForPromise(() => {
-      const goodFile = path.join(__dirname, 'fixtures/clean_workflows', 'ci.yaml');
+      const goodFile = path.join(__dirname, 'fixtures/.github/workflows', 'clean.yaml');
       return atom.workspace.open(goodFile).then(editor =>
         lint(editor).then(messages => {
           expect(messages.length).toEqual(0);
